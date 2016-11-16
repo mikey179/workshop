2ca4f6599d0764d7710205e891a42836c2f1d838
Frank Kleine <frank.kleine@1und1.de>
2016-11-16 14:16:01 +0100

    use modified server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World from proxied server\n');
 });
 
 server.listen(port, hostname, () => {
